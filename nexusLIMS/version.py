ab175fc30460231f6d4d4bcb60e2438c3a9a5c74
Josh Taillon <josh@datasophos.co>
2026-02-06 21:19:49 -0700

    Bump version to 2.4.2.dev0 for development

--- nexusLIMS/version.py
+++ nexusLIMS/version.py
@@ -6,4 +6,4 @@ __version__ = importlib.metadata.version("nexusLIMS")
 
 # Release date for the current version (YYYY-MM-DD format)
 # Set to None for unreleased development versions
-__release_date__ = "2026-02-06"
+__release_date__ = None
